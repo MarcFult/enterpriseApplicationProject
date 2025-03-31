d4119a23609f30ce7dc9f067971434ff06a77cd1
Marc Fulterer <fulterer.marc@gmail.com>
2025-03-31 11:47:39 +0200

    Test

--- enterpriseApplicationProject/sampleOne/src/main/java/com/example/sampleOne/models/sample.java
+++ enterpriseApplicationProject/sampleOne/src/main/java/com/example/sampleOne/models/sample.java
@@ -1,6 +1,7 @@
 package com.example.sampleOne.models;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,12 @@ public class sample {
 
     @GetMapping
     public String GetMessage() {
-        return "Hello world! I live!!!!!!!!!";
+        return "This is a test";
+    }
+
+    @PostMapping()
+    public void PostMessage() {
+
     }
 
 }
